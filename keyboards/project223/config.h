9914e42d74c3548110536b00ca2b08a761cecde0
Julia Ebert <julia@juliaebert.com>
2019-12-13 18:06:46 -0500

    Everything except unicode works.
    This commit message was typed on the new, mostly functional keyboard.

--- keyboards/project223/config.h
+++ keyboards/project223/config.h
@@ -42,11 +42,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 #define MATRIX_ROW_PINS \
-    { F7, F6, F5, F4, F1, F0 }
+    { D0, B7, B3, B2, B1, B0 }
 #define MATRIX_COL_PINS \
-    { B4, D7, D4, D5, C7, C6, D3, D2, D1, D0, B7, B3, B2, B1, B0 }
+    { D1, D2, D3, C6, C7, F0, F1, F4, F5, F6, F7, B6, B5, B4, D7 }
 #define UNUSED_PINS \
-    { B6, B5, B4, D6 }
+    { D4, D5, D6 }
 
 /* COL2ROW, ROW2COL*/
 #define DIODE_DIRECTION COL2ROW
